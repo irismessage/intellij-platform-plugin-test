52cf6c3e593a15f64ccf10604ac207253e5da272
imcb <irismessage@protonmail.com>
2025-02-15 16:49:04 +0000

    Make popup an action

---
+++ src/main/kotlin/com/github/irismessage/intellijplatformplugintest/DummyDialogAction.kt
@@ -0,0 +1,35 @@
+package com.github.irismessage.intellijplatformplugintest
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
+
+private class DummyDialogWrapper(project: Project?) : DialogWrapper(project) {
+    init {
+        title = ApplicationNamesInfo.getInstance().fullProductName
+        init()
+    }
+
+    override fun createCenterPanel(): JComponent {
+        val dialogPanel = JPanel(BorderLayout())
+
+        val label = JLabel(MyBundle.message("mix"))
+        label.preferredSize = Dimension(100, 50)
+        dialogPanel.add(label, BorderLayout.CENTER)
+
+        return dialogPanel
+    }
+}
+
+class DummyDialogAction() : AnAction() {
+    override fun actionPerformed(event: AnActionEvent) {
+        DummyDialogWrapper(event.project).show()
+    }
+}
